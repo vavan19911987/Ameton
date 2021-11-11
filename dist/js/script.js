9b2a1b50d5e4527fdc2b17256e1298deef6846e4
Vavan19911987 <vavan7520@gmail.com>
2021-11-11 19:28:10 +0300

    сделал первый блок и адаптив

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function () {
+    "user strict";
+
+    // const del = document.querySelector('.header');
+
+    // if (width == 660) {
+    //     del.style.background = 'none';
+    //     del.style.background = '#93BBEA';
+    // }
+
+});
+
+
+
+
+
