eb7b899dbde6ef204b054de42efa3c28bbb201ad
Vavan19911987 <vavan7520@gmail.com>
2021-11-13 12:49:51 +0300

    сделал адаптив трех секций

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,47 @@
 window.addEventListener('DOMContentLoaded', function () {
-    "user strict";
+    // "user strict";
 
-    // const del = document.querySelector('.header');
+    const slider = tns({
+        container: '.carousel',
+        items: 4,
+        slideBy: 'page',
+        autoplay: false,
+        nav: false,
+        controls: false,
+        speed: 2000,
+        responsive: {
+            320: {
+                nav: true,
+                items: 1,
 
-    // if (width == 660) {
-    //     del.style.background = 'none';
-    //     del.style.background = '#93BBEA';
-    // }
+            },
+            576: {
+                nav: false,
+                items: 2,
+
+            },
+            768: {
+                nav: false,
+                items: 2,
+
+            },
+            992: {
+                nav: false,
+                items: 3,
+
+            },
+            1200: {
+                nav: false,
+                items: 4,
+
+            },
+           
+        }
+    });
+
+    document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+    });
 
 });
 
